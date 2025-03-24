05318a690b17b142eeabea26ae96586fea6e5059
Rohan Salunke <rohans16996@gmail.com>
2025-03-25 01:39:52 +0530

    fix: fixed add func to support custom delimiter

--- src/util/add/add.ts
+++ src/util/add/add.ts
@@ -1,7 +1,15 @@
-const SERPARTOR = /,|\n/
+const DEFAULT_DELIMITER = /,|\n/
 
 export function add(input: string): number {
-    return input.split(SERPARTOR).reduce((acc, num) => {
+    let inputString = input;
+    let delimeter = DEFAULT_DELIMITER
+    if(inputString.startsWith('//')) {
+        const [customDelimiter, customString] = inputString.split('\n')
+        delimeter = new RegExp(customDelimiter.slice(2))
+        inputString = customString
+    }
+
+    return inputString.split(delimeter).reduce((acc, num) => {
         return acc + Number(num)
     }, 0)
 }
