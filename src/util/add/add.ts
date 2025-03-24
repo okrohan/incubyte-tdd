5abe602a2c9125892be7818f5ed5d1f7ac00a7ef
Rohan Salunke <rohans16996@gmail.com>
2025-03-25 01:59:00 +0530

    fix: #8 updates add fun to accept multiple custom delimiters

--- src/util/add/add.ts
+++ src/util/add/add.ts
@@ -1,12 +1,26 @@
 const DEFAULT_DELIMITER = /,|\n/
+const BRACKED_CONTENT_REGEX = /\[([^\]]+)\]/g
 const NUMBER_LIMIT = 1000
+
+function _escapeRegExpString(string: string): string {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+function _processCustomDelimeter(customDelimiter: string): RegExp {
+    if(customDelimiter.startsWith('[')) {
+        const delimiterMatches = customDelimiter.match(BRACKED_CONTENT_REGEX) as string[];
+        const delimiters = delimiterMatches.map(d => d.slice(1, -1));
+        return new RegExp(delimiters.map(d => _escapeRegExpString(d)).join("|")); // Combine into regex  
+    }
+    return new RegExp(_escapeRegExpString(customDelimiter))
+}
 
 export function add(input: string): number {
     let inputString = input;
     let delimeter = DEFAULT_DELIMITER
     if(inputString.startsWith('//')) {
         const [customDelimiter, customString] = inputString.split('\n')
-        delimeter = new RegExp(customDelimiter.slice(2))
+        delimeter = _processCustomDelimeter(customDelimiter.slice(2))
         inputString = customString
     }
 
